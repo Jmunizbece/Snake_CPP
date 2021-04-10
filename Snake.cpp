c8d5c322a4e4721aa7d99d5d38bc869503d08dab
Jmunizbece <jmunizbece@icloud.com>
2021-04-10 10:55:38 -0400

    Addition of Relevant Variables and Setting of Them
    Completion of the setup function to setup the basics of the game by doing the following:
    score set to zero, x and y coordinates of snake head set, random placement of fruit.

--- Snake.cpp
+++ Snake.cpp
@@ -2,10 +2,21 @@
 using namespace std;
 
 bool gameover;
+const int width = 20;
+const int height = 20;
+int x, y, fruitx, fruity, score;
+enum eDirection( STOP = 0, LEFT, RIGHT, UP, DOWN);
+eDirection dir;
 
 void Setup()
 {
 	gameover = false;
+	dir = STOP;
+	x = width / 2;
+	y = height / 2;
+	fruitx = rand() % width;
+	fruity = rand() % height;
+	score = 0;
 }
 
 void Draw()
