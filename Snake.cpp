a5e6d57f22cf6e21e1d995f29f8990bb96ce224a
Jmunizbece <jmunizbece@icloud.com>
2021-04-10 10:45:24 -0400

    Basic Skeleton of Snake Game Code with Functions

---
+++ Snake.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+bool gameover;
+
+void Setup()
+{
+	gameover = false;
+}
+
+void Draw()
+{
+
+}
+
+void Input()
+{
+
+}
+
+void Logic()
+{
+
+}
+
+int main()
+{
+	Setup(); 
+	while (!gameover)
+	{
+		Draw(); 
+		Input(); 
+		Logic();
+	}
+	return 0; 
+}
