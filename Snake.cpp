3978a4da3a8d601bc21c79dc8ceca8cc9c590231
Jmunizbece <jmunizbece@icloud.com>
2021-04-15 16:16:49 -0400

    Completion of Draw Function

--- Snake.cpp
+++ Snake.cpp
@@ -21,7 +21,34 @@ void Setup()
 
 void Draw()
 {
+	system("cls"); //system("clear");
+	for (int i = 0; i < width; i++)
+	{
+		cout << "#" << endl;
+	}
 
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			if (j == 0)
+			{
+				cout << "#";
+			}
+			else
+			{
+				cout << " ";
+			}
+			if (j == width - 1)
+			{
+				cout << "#";
+			}
+		}
+	}
+	for (int i = 0; i < width; i++)
+	{
+		cout << "#" << endl;
+	}
 }
 
 void Input()
